4b7884ec4a914ea9a3307b2ca4131b4d3c7310b0
victor <victor.mutuma@student.moringaschool.com>
2025-03-24 11:21:36 +0300

    save completed changes

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
 /* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour) });
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour) });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(e => e.date === date);
+    let timeOut = this.timeOutEvents.find(e => e.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return this.payPerHour * hoursWorkedOnDate.call(this, date);
+}
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, employee) => total + allWagesFor.call(employee), 0);
+}
+
+// Bind functions to employee object prototype for proper `this` context
+let employeePrototype = {
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor
+};
+
+function createEmployeeRecord(data) {
+    return Object.assign(Object.create(employeePrototype), {
+        firstName: data[0],
+        familyName: data[1],
+        title: data[2],
+        payPerHour: data[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    });
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
